844e3866574d89bf44e62cc4b8bf36cc2b1e3434
i-want-syusyoku <237514071+i-want-syusyoku@users.noreply.github.com>
2026-02-05 09:22:17 +0900

    Store calculation result in variable

--- Calculate.py
+++ Calculate.py
@@ -6,17 +6,21 @@ if b>4 or b<1:
     sys.exit()
 c = float(input('number?:'))
 if b==1:
-    print(a+c)
-    print(f'{a} + {c} = {a + c}')
+    result = a + c
+    print(result)
+    print(f'{a} + {c} = {result}')
 elif b==2:
-    print(a-c)
-    print(f'{a} - {c} = {a - c}')
+    result = a - c
+    print(result)
+    print(f'{a} - {c} = {result}')
 elif b==3:
-    print(a*c)
-    print(f'{a} * {c} = {a * c}')
+    result = a * c
+    print(result)
+    print(f'{a} * {c} = {result}')
 elif b==4:
     if c==0:
         print(f"error {a} / {c} is Can't calculate")
         sys.exit()
-    print(a/c)
-    print(f'{a} / {c} = {a / c}')
+    result = a / c
+    print(result)
+    print(f'{a} / {c} = {result}')
